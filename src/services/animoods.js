e9c6587be2c085e2ccd5779e7179a970928ef36c
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-11 18:27:17 -0400

    add create controller function

---
+++ src/services/animoods.js
@@ -0,0 +1,19 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_API_URL}/api/animoods`
+
+export const create = async (animood) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(animood),
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
