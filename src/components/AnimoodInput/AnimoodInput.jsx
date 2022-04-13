df47702949d0c3a624268705935720a602ed1a71
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-13 15:50:03 -0400

    add input styling

--- src/components/AnimoodInput/AnimoodInput.jsx
+++ src/components/AnimoodInput/AnimoodInput.jsx
@@ -1,28 +1,35 @@
 const AnimoodInput = ({ form, handleChange }) => {
   return ( 
-    <>
+    <div className='input-box'>
       <label htmlFor="name">Name</label>
       <input
+      className="input"
       type="text"
       required name="name" placeholder="e.g. Naruto"
       onChange={handleChange}
       />
       <label htmlFor="theme">Theme</label>
-      <input type="text"
+      <input 
+      className="input"
+      type="text"
       required name="theme" placeholder="e.g. Determination"
       onChange={handleChange} 
       />
-      <label htmlFor="episode">Episode or Chapter</label>
-      <input type="text"
+      <label htmlFor="episode">Episode</label>
+      <input 
+      className="input"
+      type="text"
       name="episode" placeholder="e.g. ep 123 or ch 360"
       onChange={handleChange} 
       />
       <label htmlFor="story">Story</label>
-      <textarea type="text"
+      <textarea 
+      className="input"
+      type="text"
       required name="story" placeholder="What's the story behind this scene"
       onChange={handleChange}
       />
-    </>
+    </div>
    );
 }
  
--- src/components/VibeCard/VibeCard.jsx
+++ src/components/VibeCard/VibeCard.jsx
@@ -3,10 +3,10 @@ const VibeCard = (props) => {
   return ( 
     <>
       <div className="card-content">
-        <h2>Anime Name: {props.animood.name}</h2>
-        <h3>Theme: {props.animood.theme}</h3>
-        <h3>Episode/Chapter: {props.animood.episode}</h3>
-        <p>Story: {props.animood.story}</p>
+        <h2>{props.animood.name}</h2>
+        <h3>Feels~ {props.animood.theme}</h3>
+        <h3>Episode: {props.animood.episode}</h3>
+        <p>What did it make you feel?{props.animood.story}</p>
       </div>
     </>
    );
