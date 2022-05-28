61916ee1ff2662cbbbd1f9956800e307593b04a4
gkatsikis <georgioscabro@gmail.com>
2022-05-28 10:49:53 -0400

    hide icons on web view

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,13 +17,13 @@ const NavBar = ({ user, handleLogout }) => {
             <li><Link to="/profile"><span class="material-symbols-outlined icon">
             account_circle
             </span><div className="text">Profile</div></Link></li>
-            <li><Link to="/animood"><span class="material-symbols-outlined">
+            <li><Link to="/animood"><span class="material-symbols-outlined icon">
             add_circle
             </span><div className="text">Add a Mood</div></Link></li>
-            <li><Link to="/"><span class="material-symbols-outlined">
+            <li><Link to="/"><span class="material-symbols-outlined icon">
             view_carousel
             </span><div className="text">Browse Moods</div></Link></li>
-            <li><Link to="" onClick={handleLogout}><span class="material-symbols-outlined">
+            <li><Link to="" onClick={handleLogout}><span class="material-symbols-outlined icon">
             logout
             </span><div className="text">Logout</div></Link></li>
             </div>
